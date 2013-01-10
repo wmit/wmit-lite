643994f1ea92c864f4c746afc511b2f72e8b299e
Antoine <antoine@crafting-labs.fr>
2013-01-10 12:36:12 +0100

    rails new wmit-lite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WmitLite::Application.config.secret_token = '05a6642813aff4ec305aa4859a53d9c4de0da174009aee64270199ab52a83a8d53fc792be7f202f5e53df0e22262edc1fee0283f723db618c6e9c0169988603b'
